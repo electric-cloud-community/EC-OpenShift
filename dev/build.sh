50985c13a33eeb65696db9d5186c089b64adc28d
Vishal Biyani <vishal@infracloud.io>
2017-01-24 21:26:27 +0530

    First commit with skeleton files and structure

---
+++ dev/build.sh
@@ -0,0 +1,23 @@
+filePath=$(dirname $0)
+cd $filePath/..
+
+if [ $# -eq 0 ]; then
+ DEPLOY=0
+elif [ "$1" == "--deploy" ]; then
+ DEPLOY=1 
+else
+ DEPLOY=0
+fi
+
+rm ./out/EC-OpenShift.jar
+rm ./EC-OpenShift.zip
+
+jar cvf ./out/EC-OpenShift.jar dsl/ META-INF/ pages/ lib/ htdocs/
+zip -r ./EC-OpenShift.zip dsl/ META-INF/ pages/ lib/ htdocs/
+
+if [ $DEPLOY -eq 1 ]; then
+  echo "Installing plugin ..."
+  ectool --server localhost login admin changeme
+  ectool installPlugin ./out/EC-OpenShift.jar --force 1
+  ectool promotePlugin EC-OpenShift-1.0.1
+fi  
---
+++ dsl/properties/scripts/preamble.groovy
@@ -0,0 +1,4 @@
+$[/plugins/EC-Kubernetes/project/scripts/Logger]
+$[/plugins/EC-Kubernetes/project/scripts/BaseClient]
+$[/plugins/EC-Kubernetes/project/scripts/EFClient]
+$[/plugins/EC-Kubernetes/project/scripts/KubernetesClient]
