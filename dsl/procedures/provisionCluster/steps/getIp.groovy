4d909f002d5e59a7ffa2d6b2b7499b6f1f3e4387
Vishal Biyani <vishal@infracloud.io>
2017-05-16 22:08:04 +0530

    Added code to retrieve master node IP from EC-ESX plugin

--- dsl/procedures/provisionCluster/steps/getIp.groovy
+++ dsl/procedures/provisionCluster/steps/getIp.groovy
@@ -1,5 +1,21 @@
+import groovy.json.*
+
 $[/myProject/scripts/preamble]
 
-def result = doHttpGet("/rest/v1.0/myJob/ESX/vms")
 
-println result
+def jobId = "$[/myJob]"
+EFClient efClient = new EFClient()
+def result = efClient.doHttpGet("/rest/v1.0/jobs/$jobId")
+println result.data.job.jobStep[1].liveProcedureStep
+
+
+def jobStepId = result.data.job.jobStep[1].jobStepId
+println jobStepId 
+
+result = efClient.doHttpGet("/rest/v1.0/jobSteps/$jobStepId")
+
+println "----STEP Desc----"
+println result.data
+println "----STEP Desc----"
+
+println result.data.calledProcedure.jobStep.propertySheet.property[1]
