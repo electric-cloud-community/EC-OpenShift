a24be4246e8dc005c11a289689ba2cd5bd5ddac4
Vishal Biyani <vishal@infracloud.io>
2017-06-02 18:55:02 +0530

    Added Bellow Openshift deployment topologies:     * Minimal     * Multiple Etcd     * HAProxy (Multiple Masters, Multiple Etcd)

--- dsl/procedures/provisionCluster/procedure.dsl
+++ dsl/procedures/provisionCluster/procedure.dsl
@@ -16,116 +16,16 @@ procedure 'Provision Cluster',
     	  actualParameter 'additionalArtifactVersion', 'com.electriccloud:EC-OpenShift-Grapes:1.0.0'
     }
    
-    step 'Prepare Setup', 
-	  command: new File(pluginDir, 'dsl/procedures/provisionCluster/steps/prepare.groovy').text,
-	  errorHandling: 'failProcedure',
-	  exclusiveMode: 'none',
-	  postProcessor: 'postp',
-	  releaseMode: 'none',
-	  shell: 'ec-groovy',
-	  timeLimitUnits: 'minutes'
-
-
-	step 'Generate Certs',
-		command: "htpasswd -b -c passwordfile test test",
-		releaseMode: 'none',
-	    projectName: 'EC-OpenShift-1.2.0',
-	    errorHandling: 'failProcedure',
-	    condition: '$[openshiftNotPresent]',
-	    timeLimitUnits: 'minutes'
-
-
-    step 'Import Master Node',
-      command: "",
-      releaseMode: 'none',
-      projectName: 'EC-OpenShift-1.2.0',
-      subprocedure: 'Import',
-      subproject: '/plugins/EC-ESX/project',
-      errorHandling: 'failProcedure',
-      condition: '$[openshiftNotPresent]',
-      timeLimitUnits: 'minutes', {
-
-	      actualParameter 'connection_config', '$[esx_config]'
-		  actualParameter 'esx_datastore', '$[esx_datastore]'
-		  actualParameter 'esx_host', '$[esx_host]'
-		  actualParameter 'esx_number_of_vms', '1'
-		  actualParameter 'esx_properties', 'hostname=$[openshift_public_hostname]'
-		  actualParameter 'esx_vm_memory', '$[master_memory]'
-		  actualParameter 'esx_vm_num_cpus', '$[master_cpu]'
-		  actualParameter 'esx_source_directory', '/home/vagrant/CentOS7/CentOS7.ovf'
-		  actualParameter 'esx_vmname', 'OpenShift-$[openshift_public_hostname]'
-		  actualParameter 'ovftool_path', '$[ovftool_path]'
-		  actualParameter 'esx_vm_poweron', '1'
-		  actualParameter 'esx_properties_location', '/myJob/ESX'
-
-  	step 'Get Master IP', 
-	  command: new File(pluginDir, 'dsl/procedures/provisionCluster/steps/getIp.groovy').text,
-	  errorHandling: 'failProcedure',
-	  exclusiveMode: 'none',
-	  postProcessor: 'postp',
-	  releaseMode: 'none',
-	  shell: 'ec-groovy',
-	  condition: '$[openshiftNotPresent]',
-	  timeLimitUnits: 'minutes'
-
-
-	step 'Import worker Node1',
-      command: "",
-      releaseMode: 'none',
-      projectName: 'EC-OpenShift-1.2.0',
-      subprocedure: 'Import',
-      subproject: '/plugins/EC-ESX/project',
-      errorHandling: 'failProcedure',
-      condition: '$[openshiftNotPresent]',
-      timeLimitUnits: 'minutes', {
-
-	      actualParameter 'connection_config', '$[esx_config]'
-		  actualParameter 'esx_datastore', '$[esx_datastore]'
-		  actualParameter 'esx_host', '$[esx_host]'
-		  actualParameter 'esx_number_of_vms', '1'
-		  actualParameter 'esx_properties', 'hostname=$[node-1]'
-		  actualParameter 'esx_vm_memory', '$[node_memory]'
-		  actualParameter 'esx_vm_num_cpus', '$[node_cpu]'
-		  actualParameter 'esx_source_directory', '/home/vagrant/CentOS7/CentOS7.ovf'
-		  actualParameter 'esx_vmname', 'OpenShift-$[node-1]'
-		  actualParameter 'ovftool_path', '$[ovftool_path]'
-		  actualParameter 'esx_vm_poweron', '1'
-    
-    }
-
-    step 'Import worker Node2',
-      command: "",
-      releaseMode: 'none',
-      projectName: 'EC-OpenShift-1.2.0',
-      subprocedure: 'Import',
-      subproject: '/plugins/EC-ESX/project',
-      errorHandling: 'failProcedure',
-      condition: '$[openshiftNotPresent]',
-      timeLimitUnits: 'minutes', {
-
-	      actualParameter 'connection_config', '$[esx_config]'
-		  actualParameter 'esx_datastore', '$[esx_datastore]'
-		  actualParameter 'esx_host', '$[esx_host]'
-		  actualParameter 'esx_number_of_vms', '1'
-		  actualParameter 'esx_properties', 'hostname=$[node-2]'
-		  actualParameter 'esx_vm_memory', '$[node_memory]'
-		  actualParameter 'esx_vm_num_cpus', '$[node_cpu]'
-		  actualParameter 'esx_source_directory', '/home/vagrant/CentOS7/CentOS7.ovf'
-		  actualParameter 'esx_vmname', 'OpenShift-$[node-2]'
-		  actualParameter 'ovftool_path', '$[ovftool_path]'
-		  actualParameter 'esx_vm_poweron', '1'
-    
-    }
-
-	step 'generateHostsFile', 
+   
+	step "generateHostsFile", 
 	  command: new File(pluginDir, 'dsl/procedures/provisionCluster/steps/provisionCluster.groovy').text,
 	  errorHandling: 'failProcedure',
 	  exclusiveMode: 'none',
 	  postProcessor: 'postp',
 	  releaseMode: 'none',
 	  shell: 'ec-groovy',
-	  condition: '$[openshiftNotPresent]',
 	  timeLimitUnits: 'minutes'
+
 
 	step 'provisionCluster', 
 	  command: "",
@@ -133,35 +33,6 @@ procedure 'Provision Cluster',
 	  exclusiveMode: 'none',
 	  postProcessor: 'postp',
 	  releaseMode: 'none',
-	  condition: '$[openshiftNotPresent]',
 	  timeLimitUnits: 'minutes'
-	
-    def project_name = '$[project]'
-	step 'configureCluster', 
-	  command: "ansible-playbook $pluginDir/ansible-scripts/get_service_token.yml -i /tmp/hosts --extra-vars \"project_name=$project_name\"",
-	  errorHandling: 'failProcedure',
-	  exclusiveMode: 'none',
-	  postProcessor: "postp --load $pluginDir/dsl/postp_matchers.pl",
-	  releaseMode: 'none',
-	  timeLimitUnits: 'minutes'
-
-	step 'createPluginConfiguration', 
-	  command: new File(pluginDir, 'dsl/createPluginConfig.dsl').text,	 
-	  errorHandling: 'failProcedure',
-	  exclusiveMode: 'none',
-	  shell: 'ectool evalDsl --dslFile {0}',
-	  postProcessor: "postp",
-	  releaseMode: 'none',
-	  timeLimitUnits: 'minutes'
-
-
-    step 'Wait for PluginConfig to populate',
-        command: "sleep 30s",
-        releaseMode: 'none',
-        projectName: 'EC-OpenShift-1.2.0',
-        errorHandling: 'failProcedure',
-        condition: '$[openshiftNotPresent]',
-        timeLimitUnits: 'minutes'
 	
 }
-  
--- dsl/procedures/provisionCluster/steps/provisionCluster.groovy
+++ dsl/procedures/provisionCluster/steps/provisionCluster.groovy
@@ -1,95 +1,116 @@
 $[/myProject/scripts/preamble]
 
+/*
+// Get Master VM IP from Properties set by EC-ESX plugin
 def jobId = "$[/myJob]"
 EFClient efClient = new EFClient()
 def result
-
 def queryArgs = [
                 jobId: jobId
         ]
 
 result = efClient.doHttpGet("/rest/v1.0/properties/OpenShiftMasterIP",true, queryArgs)
-
 def openShiftMasterIP=result.data.property.value
 println openShiftMasterIP
+*/
 
-def pluginProjectName = '$[/myProject/projectName]'
+
+def openShiftMasterIP="1.2.3.4"
+
 // Input parameters
-def configName = '$[config]'
-
-def openshift_hostname = '$[openshift_hostname]'
-def openshift_public_hostname = '$[openshift_public_hostname]'
+def pluginProjectName = '$[/myProject/projectName]'
+def hostname_prefix = '$[openshift_hostname_prefix]'
 def domain_name = '$[domain_name]'
-//def openshift_ip = '127.0.0.1'
-def openshift_ip = openShiftMasterIP
-def openshift_public_ip = openShiftMasterIP
-String[] nodes = '$[openshift_nodes]'.split(',')
+def user_login = '$[user_login]'
+def workspaceDir = System.getenv("COMMANDER_WORKSPACE")
+def additional_children = ""
+def additional_vars = ""
+
+// Prepare default masterList
+def master_hostname = "${hostname_prefix}master.${domain_name}"
+def masterList = "$master_hostname openshift_ip=\"$openShiftMasterIP\" openshift_public_ip=\"$openShiftMasterIP\" openshift_node_labels=\"{'region':'infra','zone':'default'}\" openshift_hostname=\"${master_hostname}\" openshift_public_hostname=\"${master_hostname}\" containerized=\"true\" deployment_type=origin\n"
+
+// Prepare default etcdList
+def etcdList = "${hostname_prefix}master.${domain_name}\n"
+
+// Prepare default nodeList
+int no_openshift_nodes = '$[no_openshift_nodes]' as Integer
 def nodeList = ""
-for (String node: nodes) {
-    nodeList = nodeList + "${node}.${domain_name} deployment_type=origin\n".toString()
+for (int i = 0; i<no_openshift_nodes; i++) {
+    nodeList += "${hostname_prefix}node${i}.${domain_name} deployment_type=origin\n".toString()
 }
 
 
-def user_login = '$[user_login]'
-def workspaceDir = System.getenv("COMMANDER_WORKSPACE")
+// Prepare default LB (loadbanacer) list
+def lbList = ""
 
-def text ="""[OSEv3:children]
-masters
-nodes
- 
- 
-[OSEv3:vars]
-ansible_ssh_user=$user_login
-ansible_become=true
-openshift_master_htpasswd_file=$workspaceDir/passwordfile
-deployment_type=origin
-containerized=true
-openshift_release=\"1.4.0\"
-openshift_master_cluster_hostname=${openshift_hostname}.${domain_name}
-openshift_master_cluster_public_hostname=${openshift_public_hostname}.${domain_name}
-osm_cluster_network_cidr=\"10.130.0.0/14\"
- 
-[masters]
-$openshift_hostname.${domain_name} openshift_ip=\"$openshift_ip\" openshift_public_ip=\"$openshift_public_ip\" openshift_node_labels=\"{'region':'infra','zone':'default'}\" openshift_hostname=\"${openshift_hostname}.${domain_name}\" openshift_public_hostname=\"${openshift_public_hostname}.${domain_name}\" containerized=\"true\" deployment_type=origin
- 
- 
-[etcd]
-$openshift_hostname.${domain_name} openshift_ip=$openshift_public_ip
 
-[nodes]
-${openshift_public_hostname}.${domain_name} deployment_type=origin
-$nodeList"""
+// Define Additional ansible hosts file elements based on topology used
 
-def binding = ["openshift_hostname":openshift_hostname, 
-			   "openshift_public_hostname":openshift_public_hostname,
-			   "openshift_ip":openshift_ip,
-			   "openshift_public_ip":openshift_public_ip,
-			   "user_login": user_login]
+switch('$[topology]') {
+
+	case 'haproxy':
+
+        additional_children = "etcd\nlb"
+	
+	    master_hostname = '$[openshift_hostname_prefix]' + "lb." + '$[domain_name]'
+	    lbList = "[lb]\n" + master_hostname + "\n"
+
+	    additional_vars = """openshift_master_cluster_method=native
+						|openshift_node_kubelet_args={'pods-per-core': ['10'], 'max-pods': ['250'], 'image-gc-high-threshold': ['90'], 'image-gc-low-threshold': ['80']}
+						|openshift_clock_enabled=true""".stripMargin()
+
+	    // construct etcd list
+	    etcdList = ""
+	    for(int i=0;i<3;i++){
+		    etcdList += "${hostname_prefix}etcd${i}.${domain_name}\n".toString()
+	    }
+
+		// Construct masterList and nodeList	
+		def master = ""				
+		masterList = ""
+		for(int i=0;i<3;i++){
+			master= "${hostname_prefix}master${i}.${domain_name}"
+			masterList += "$master openshift_ip=\"$openShiftMasterIP\" openshift_public_ip=\"$openShiftMasterIP\" openshift_node_labels=\"{'region':'infra','zone':'default'}\" openshift_hostname=\"${master_hostname}\" openshift_public_hostname=\"${master_hostname}\" containerized=\"true\" deployment_type=origin\n".toString()
+			nodeList += "${master} deployment_type=origin\n".toString()
+		}
+			
+		break
+
+	case 'multipleEtcd':
+
+        additional_children = "etcd"
+        etcdList = ""
+	    for(int i=0;i<3;i++){
+		    etcdList += "${hostname_prefix}etcd${i}.${domain_name}\n".toString()
+	    }
+	    nodeList += "${master_hostname} deployment_type=origin\n".toString()
+	    break
+
+	default:
+	 	nodeList += "${master_hostname} deployment_type=origin\n".toString()
+	 	break
+}
+
+
+// Read ansible hosts template
+File hostTemplate = new File(System.getenv("COMMANDER_PLUGINS") + "/EC-OpenShift-1.2.0/dsl/procedures/provisionCluster/steps/ansible_hosts_template")
+templateText = hostTemplate.text
+
+def binding = ["additional_children":additional_children, 	   
+			   "additional_vars":additional_vars,
+			   "masterList":masterList,
+			   "etcdList": etcdList,
+			   "nodeList":nodeList,
+			   "lbList":lbList,
+			   "master_hostname":master_hostname,
+			   "workspaceDir":workspaceDir,
+			   "user_login":user_login]
 
 def engine = new groovy.text.SimpleTemplateEngine() 
-def template = engine.createTemplate(text).make(binding) 
+def template = engine.createTemplate(templateText).make(binding) 
 
 println template
 
 File hostsFile = new File('/tmp/hosts')
 hostsFile.text = template
-
-/*
-EFClient efClient = new EFClient()
-def pluginConfig = efClient.getConfigValues('ec_plugin_cfgs', configName, pluginProjectName)
-
-String accessToken = 'Bearer ' + pluginConfig.credential.password
-String clusterEndpoint = pluginConfig.clusterEndpoint
-
-String openshiftHealthUrl = "$clusterEndpoint"
-
-OpenShiftClient client = new OpenShiftClient()
-def resp = client.checkClusterHealth(openshiftHealthUrl, accessToken)
-if (resp.status == 200){ 
-	efClient.logger INFO, "The service is reachable at ${clusterEndpoint}"
-}
-if (resp.status >= 400){
-	efClient.handleProcedureError("The Kubernetes cluster at ${clusterEndpoint} was not reachable. Health check at $openshiftHealthUrl failed with $resp.statusLine")
-}
-
-*/
