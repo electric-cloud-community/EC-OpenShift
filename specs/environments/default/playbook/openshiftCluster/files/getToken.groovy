ce7984c118e6de31b8f26b45e76f16088e50175b
Polina Shubina <925043@gmail.com>
2018-06-14 15:54:45 +0300

    Ansible playbook for openshift

---
+++ specs/environments/default/playbook/openshiftCluster/files/getToken.groovy
@@ -0,0 +1,28 @@
+import groovy.json.JsonSlurper
+
+def shellPath = args[0]
+println shellPath
+if (!shellPath) {
+    println "No shell path was provided"
+    System.exit(1)
+}
+def clusterIpPort = '10.200.1.106:8443'
+def username = 'admin'
+def password = 'admin'
+def projectName = 'flowqe-test-project'
+def serviceAccountName = 'flowqe'
+def configPath = '/var/lib/origin/openshift.local.config/master/admin.kubeconfig'
+
+// Login using cluster admin credentials
+"${shellPath} login $clusterIpPort --username=$username --password=$password".execute().text
+
+// Describe the service account to discover the secret token name
+def _token
+"${shellPath} describe secret $serviceAccountName".execute().text.eachLine {
+    if (it =~ /token/) {
+        _token = it.tokenize().last().trim()
+    }
+}
+
+println _token
+
