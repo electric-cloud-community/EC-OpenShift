1be07d95f122388172b9d0648c3318c1dc134145
Polina Shubina <925043@gmail.com>
2018-06-19 16:31:54 +0300

    Custom promotion script

---
+++ specs/environments/default/setupEnv.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+use ElectricCommander;
+
+
+my $jobId = $ENV{COMMANDER_JOBID};
+my $jobStepId = $ENV{COMMANDER_JOBSTEPID};
+
+print "Job Step ID: $jobStepId\n";
+for (keys %ENV) {
+    if (/^COMMANDER/) {
+        delete $ENV{$_};
+    }
+}
+
+my $efServer = 'localhost';
+my $ec = ElectricCommander->new({server => $efServer, debug => 1});
+my $plugins = $ec->getPlugins();
+my @dependencies = qw(EC-Kubernetes);
+my @pluginNames = ();
+for my $plugin ($plugins->findnodes('//plugin')) {
+    my $name = $plugin->findvalue('pluginName');
+    my $key = $plugin->findvalue('pluginKey');
+    if (grep { $_ eq $key } @dependencies) {
+        push @pluginNames, "$name";
+        print "$name\n";
+    }
+}
+
+for my $plugin (@pluginNames) {
+    print "Promoting plugin $plugin\n";
+    my $start = time;
+    $ec->promotePlugin({pluginName => $plugin, promoted => 0});
+    $ec->promotePlugin($plugin);
+    my $end = time;
+    my $eta = $end - $start;
+    print "Done with plugin $plugin in $eta seconds\n";
+}
+
