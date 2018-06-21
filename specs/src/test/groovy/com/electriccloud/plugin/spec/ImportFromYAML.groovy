0df1cb33d79836de25001fd72ffb6c8523f0841f
Polina Shubina <925043@gmail.com>
2018-06-21 15:23:19 +0300

    cleanup

--- specs/src/test/groovy/com/electriccloud/plugin/spec/ImportFromYAML.groovy
+++ specs/src/test/groovy/com/electriccloud/plugin/spec/ImportFromYAML.groovy
@@ -262,11 +262,81 @@ class ImportFromYAML extends OpenShiftHelper {
         deleteService(projectName, serviceName)
     }
 
+    def "many top-level services"() {
+        given:
+        def sampleOneName = 'my-service1-nginx-deployment1'
+        def sampleTwoName = 'my-service2-nginx-deployment2'
+        def template = getTemplate('twoTopLevel.yaml', [:])
+        when:
+        def result = runProcedureDsl """
+                runProcedure(
+                    projectName: '$projectName',
+                    procedureName: 'Import Microservices',
+                    actualParameter: [
+                        osTemplateYaml: '''$template''',
+                        projName: '$projectName',
+                        envProjectName: '$projectName',
+                        envName: '$envName',
+                        clusterName: '$clusterName'
+                    ]
+                )
+            """
+        then:
+        logger.debug(result.logs)
+        def serviceOne = getService(
+            projectName,
+            sampleOneName,
+            clusterName,
+            envName
+        )
+        def serviceTwo = getService(
+            projectName,
+            sampleTwoName,
+            clusterName,
+            envName
+        )
+
+
+        assert result.outcome != 'error'
+        assert serviceOne.service
+        assert serviceOne.service.defaultCapacity == '3'
+        assert serviceTwo.service
+        assert serviceTwo.service.defaultCapacity == '3'
+        def containersOne = serviceOne.service.container
+        assert containersOne.size() == 2
+        assert containersOne[0].containerName == 'nginx'
+        assert containersOne[0].imageName == 'nginx'
+        assert containersOne[0].imageVersion == '1.7.9'
+        assert containersOne[1].containerName == 'nginx2'
+        assert containersOne[1].imageName == 'nginx'
+        assert containersOne[1].imageVersion == '1.7.10'
+        def portOne1 = containersOne[0].port[0]
+        assert portOne1
+        assert portOne1.containerPort == '80'
+        def portOne2 = containersOne[1].port[0]
+        assert portOne2
+        assert portOne2.containerPort == '90'
+        def containersTwo = serviceTwo.service.container
+        assert containersTwo.size() == 1
+        assert containersTwo[0].containerName == 'nginx'
+        assert containersTwo[0].imageName == 'nginx'
+        assert containersTwo[0].imageVersion == '1.7.9'
+        def portTwo = containersTwo[0].port[0]
+        assert portTwo
+        assert portTwo.containerPort == '80'
+        cleanup:
+        deleteService(projectName, sampleOneName)
+        deleteService(projectName, sampleTwoName)
+    }
+
+
     def getMappingDetail(service, name) {
       def parameterDetail = service.service?.parameterDetail.find {
         it.parameterName == name
       }.parameterValue
       return parameterDetail
     }
+
+
 
 }
